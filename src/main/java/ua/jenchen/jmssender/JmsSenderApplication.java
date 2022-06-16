f74b44835e4b6bb23c294ddc606408b4a047cb49
Evgeniy Zadoyannyy <evgeniy_zadoyannyy@epam.com>
2022-06-16 21:51:36 +0300

    Initial commit

---
+++ src/main/java/ua/jenchen/jmssender/JmsSenderApplication.java
@@ -0,0 +1,13 @@
+package ua.jenchen.jmssender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsSenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmsSenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/jenchen/jmssender/JmsSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.jenchen.jmssender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsSenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
